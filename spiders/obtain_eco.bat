045482ca28d5a9f8fe1fd986ddb9e3ee98fb5d89
guaaan <juampablo.txt@gmail.com>
2025-04-14 11:11:27 -0400

    estos bat solo son para windows

--- spiders/obtain_eco.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Users\JUAN.RODRIGUEZ\Documents\repos\scraper\spiders
-C:\Users\JUAN.RODRIGUEZ\appdata\Roaming\Python\Python310\Scripts\scrapy runspider .\eco.py -O ../outputs/Eitems.json
