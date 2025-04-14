0995f6df08e23653671116e2937193f609f4de17
Juan Rodriguez <juan.rodriguez@redfarma.cl>
2025-04-14 11:04:00 -0400

    estos bat son solo para windows

--- obtener/obtain_ahumada.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo C:\Program Files\Python310C:\python.exe >archivotemporalahumada.ps1
-echo C:\price-scraper\venv\Scripts\activate.bat >archivotemporalahumada.ps1
-echo C:\price-scraper\obtener\ahumada.py >archivotemporalahumada.ps1
-powershell.exe -ExecutionPolicy Bypass -File "archivotemporalahumada.ps1"
-del archivotemporalahumada.ps1
--- obtener/obtain_itemsfarmex.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo C:\Program Files\Python310C:\python.exe >farmex.ps1
-echo C:\price-scraper\venv\Scripts\activate.bat >farmex.ps1
-echo C:\price-scraper\obtener\detailedfarmex.py >farmex.ps1
-powershell.exe -ExecutionPolicy Bypass -File "farmex.ps1"
-del farmex.ps1
